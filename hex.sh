8260a21ddedb515823171efdbfcbbfed03577753
Andrew Barlow <dandrewbarlow@gmail.com>
2022-06-27 17:03:55 -0600

    committing b4 trying go instead

--- hex.sh
+++ hex.sh
@@ -15,16 +15,22 @@
 # help
 
 
-SHORT=f:,r:,h
-LONG=file:,rate,help
+SHORT=f:,h
+LONG=file:,help
 OPTS="$(getopt --options $SHORT --longoptions $LONG -- $@ )"
 
 # display options
 help() {
 	echo "Options:"
 	echo "-f (--file) <file> 	| define source image"
-	echo "-r (--rate) <rate> 	| set image corruption rate"
+	# echo "-r (--rate) <rate> 	| set image corruption rate"
 	echo "-h (--help) 		| display help"
+}
+
+# random_num min max
+random_num() {
+	local result="$((RANDOM % $2 + $1))"
+	echo "$result"
 }
 
 # Returns the count of arguments that are in short or long options
@@ -62,15 +68,36 @@ do
 	esac
 done
 
-if [ "$file" ] && [ "$rate" ]
+if [ "$file" ]
 then
-	# get hex content of file
-	hex="$(hexdump $file)"
 
-	# TODO figure out why my edits r breaking png format
+	tmp="$(mktemp /tmp/hex.XXXXXX)"
 
-	# POTENTIAL FIX - but still not recognized :(
-	# https://stackoverflow.com/questions/7826526/transform-hexadecimal-information-to-binary-using-a-linux-command
-	# xxd -r -p hex output.png
+	let i=0
+
+	# get hex content of file, pipe to a line-by-line read loop
+	hexdump $file |
+
+	while read -r line; do
+
+		let i++
+
+		chance="$(random_num 0 100000)"
+
+		if [ "$chance" -lt 5 ] && [ "$i" -gt 500 ]
+		then
+			echo "$(sed 's/0/1/' <<< $line)" >> "$tmp"
+		else 
+			echo "$line" >> "$tmp"
+		fi
+
+	done
+
+
+	# TODO - handle extensions
+	xxd -r -p "$tmp" output.jpg
+
+	rm "$tmp"
+
 
 fi
